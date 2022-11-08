e686d5ce06081962b2bec268fa8cf9a455ba99cd
sebastian <sebastian.danielsson@protonmail.com>
2022-11-08 13:25:45 +0100

    Exclude tailwind.config.js from content path to avoid error message
    Fix found at: https://github.com/tailwindlabs/tailwindcss/discussions/8650#discussioncomment-3048152

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'media',
-  content: ["./**/*.{html,js}"],
+  content: ["./src/*.{html,js}"],
   theme: {
     extend: {},
   },
