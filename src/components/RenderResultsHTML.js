4979eacdaf84604052b90c297f72763251cf97b2
sebastian <sebastian.danielsson@protonmail.com>
2022-12-02 16:43:04 +0100

    Fix bookCover size

--- src/components/RenderResultsHTML.js
+++ src/components/RenderResultsHTML.js
@@ -2,8 +2,8 @@ const RenderResultsHTML = (searchResults) => {
     let resultsHTML = '';
     for (let i=0; i<searchResults.length; i++) {
         resultsHTML += `
-            <li class="flex flex-row p-4 rounded-lg dark:hover:bg-gray-600">
-                <img class="max-h-32 max-w-max rounded-lg mr-4" src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img,w_512/https://blog.snappa.com/wp-content/uploads/2022/01/ebook-cover-size-guide.jpg" alt="${searchResults[i].title} cover">
+            <li class="flex flex-row p-4 gap-4 rounded-lg dark:hover:bg-gray-600">
+                <img class="max-w-[84px] aspect-[21/32] rounded-lg" src="${searchResults[i].coverImage}" alt="${searchResults[i].title} cover">
                 <div class="flex flex-col justify-center">
                     <span class="text-2xl">${searchResults[i].title}</span>
                     <a href="./?q=${searchResults[i].author}" class="text-base text-purple-400">${searchResults[i].author}</a>
