73c7a61cd79d6c0dcd49a45373fed30394551ff6
sebastian <sebastian.danielsson@protonmail.com>
2022-12-02 12:33:47 +0100

    feat: Set searchQuery in url, get url quert when there are more than 1 param

--- src/script.js
+++ src/script.js
@@ -2,32 +2,40 @@
 
 // Load bookList on page load and call getURLQuery
 let bookList = [];
-
 window.addEventListener('load', () => {
   getAll().then((apiBooks) => (bookList = apiBooks) && getURLQuery());
 });
 
+// Show loading spinner
 function showLoading() {
     loadingSpinner.classList.remove('hidden');
 }
 
+// Hide loading spinner
 function hideLoading() {
     loadingSpinner.classList.add('hidden');
 }
 
-// Search on input
-searchField.oninput = function(e) {filterResults(e.target.value)};
-
-// Get URL search query
+// Get search query from URL
 function getURLQuery() {
     hideLoading();
-    let searchTerm = '';
-    if (window.location.search.includes('searchQuery=')) {
-        searchTerm = decodeURI(window.location.search.replace("?searchQuery=", ""));
-        searchField.value = searchTerm;
-        filterResults(searchTerm);
-    }
+    const urlParams = new URLSearchParams(window.location.search);
+    let searchTerm = urlParams.get('q');
+    searchField.value = searchTerm;
+    filterResults(searchTerm);
 }
+
+// Put search query in URL
+function setURLQuery(searchTerm) {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set('q', searchTerm);
+    window.history.replaceState({}, '', `${window.location.pathname}?${urlParams}`);
+}
+
+// Capture search input
+searchField.oninput = function(e) {
+    filterResults(e.target.value), setURLQuery(e.target.value);
+};
 
 // Filter results
 function filterResults(searchTerm) {
