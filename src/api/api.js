ad80c36404ca07e8873afd6e87334ac47e30726e
Sebastian <sebastian.danielsson@proton.me>
2023-03-28 20:06:59 +0200

    Linting fixes

--- src/api/api.js
+++ src/api/api.js
@@ -1,15 +1,15 @@
-const url = 'https://gik2f8-labs.herokuapp.com/books/';
+const url = "https://gik2f8-labs.herokuapp.com/books/";
 
 async function getAll() {
-    const result = await fetch(url)
-        .then((result) => result.json())
-        .catch((e) => e);
-    return result;
+  const result = await fetch(url)
+    .then((result) => result.json())
+    .catch((e) => e);
+  return result;
 }
 
 async function getItem(id) {
-    const result = await fetch(url+id)
-        .then(result => result.json())
-        .catch(e => e);
-    return result;
+  const result = await fetch(url + id)
+    .then((result) => result.json())
+    .catch((e) => e);
+  return result;
 }
